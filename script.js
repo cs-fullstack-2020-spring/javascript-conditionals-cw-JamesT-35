e98e18176ca7fe60501ed75b3087685ebd17e764
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-17 14:01:14 -0600

    conditionals

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 //name = "Hello World"
 //console.log(name)
 
-let anyWord = prompt("What is the word?");
-let theSameWord = (anyWord == theSameWord)
-if (anyWord == theSameWord) {
-    console.log("THAT IS CORRECT");
-} else (anyWord != anyWord) {
-    console.log("THAT IS NOT CORRECT")
-}
+//let anyWord = prompt("What is the word?");
+//let theSameWord = prompt("What is the word?");
+//if (anyWord == theSameWord) {
+//    console.log("THAT IS CORRECT")
+//} else {
+ //   console.log("THAT IS NOT CORRECT")
+//}
+
+  //let number1 = prompt("Enter a number");
+ // if (number1 >= 0) {
+  //    console.log( number1 +" is positive")
+ // } else if (number1 < 0) {
+  //    console.log(number1 + " is negative")
+ // }
   
+ let number1 = prompt("Enter a number")
+ let number2 = prompt("Enter another number")
+ if(Sum1 == )
+ Sum1 = (parseInt(number1) + parseInt(number2) = )
+
+ let alert1 = alert("Sum of numbers are even")
+
+
+
+
+
+
 
     
 
