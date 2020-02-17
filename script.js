88e2c042d99024575919b25a1426d0ff266382f2
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-17 14:50:56 -0600

    Classwork revised

--- script.js
+++ script.js
@@ -16,16 +16,15 @@
   //    console.log(number1 + " is negative")
  // }
   
- let number1 = prompt("Enter a number");
- let number2 = prompt("Enter another number");
- Sum1 = (parseInt(number1) + parseInt(number2))
- let alert1 = alert("Sum of numbers are even");
- let alert2 = alert("Sum of numbers are odd");
- if(Sum1 /= 2) {
-     console.log(Sum1 + alert1)
- } else if (Sum1 !== 2 ) {
-    console.log(Sum1 + alert2)
- }
+ //let number1 = prompt("Enter a number");
+ //let number2 = prompt("Enter another number");
+ //Sum1 = (parseInt(number1) + parseInt(number2) )
+ //let alert1 = alert("Sum of numbers are even");
+ //let total = Sum1 /= 2
+ //if(total) {
+   // console.log(total)
+ //}
+ //let alert2 = alert("Sum of numbers are odd");
      
  
 
