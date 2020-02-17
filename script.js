d13292408af82f0a4aa87afb4751eafeea147c42
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-17 13:26:29 -0600

    javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+//name = "Hello World"
+//console.log(name)
+
+let anyWord = prompt("What is the word?");
+let theSameWord = (anyWord == theSameWord)
+if (anyWord == theSameWord) {
+    console.log("THAT IS CORRECT");
+} else (anyWord != anyWord) {
+    console.log("THAT IS NOT CORRECT")
+}
+  
+
+    
+
