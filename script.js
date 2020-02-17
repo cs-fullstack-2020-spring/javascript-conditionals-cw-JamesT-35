bf1a9f3d16622650b2d0d50e1634775819612253
autumn-ragland <raglanda998@gmail.com>
2020-02-17 15:43:39 -0600

    graded

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 //name = "Hello World"
 //console.log(name)
 
-//let anyWord = prompt("What is the word?");
-//let theSameWord = prompt("What is the word?");
-//if (anyWord == theSameWord) {
+// let anyWord = prompt("What is the word?");
+// let theSameWord = prompt("What is the word?");
+// if (anyWord == theSameWord) {
 //    console.log("THAT IS CORRECT")
-//} else {
- //   console.log("THAT IS NOT CORRECT")
-//}
+// } else {
+//    console.log("THAT IS NOT CORRECT")
+// }
 
-  //let number1 = prompt("Enter a number");
- // if (number1 >= 0) {
-  //    console.log( number1 +" is positive")
- // } else if (number1 < 0) {
-  //    console.log(number1 + " is negative")
- // }
+//   let number1 = prompt("Enter a number");
+//  if (number1 >= 0) {
+//      console.log( number1 +" is positive")
+//  } else if (number1 < 0) {
+//      console.log(number1 + " is negative")
+//  }
   
- //let number1 = prompt("Enter a number");
- //let number2 = prompt("Enter another number");
- //Sum1 = (parseInt(number1) + parseInt(number2) )
- //let alert1 = alert("Sum of numbers are even");
- //let total = Sum1 /= 2
- //if(total) {
-   // console.log(total)
- //}
- //let alert2 = alert("Sum of numbers are odd");
+//  let number1 = prompt("Enter a number");
+//  let number2 = prompt("Enter another number");
+// //  variable names should start with a lower case letter
+//  Sum1 = (parseInt(number1) + parseInt(number2) )
+//  let alert1 = alert("Sum of numbers are even");
+//  let total = Sum1 /= 2
+// //  two numbers are EVEN if they are divisible by 2 with NO REMAINDER 
+//  if(total) {
+//    console.log(total)
+//  }
+//  let alert2 = alert("Sum of numbers are odd");
      
  
 
